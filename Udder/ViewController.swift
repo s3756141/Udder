00702b1b82e83de05f14a810cfcd1c9ce1a63f35
Bryan <s3756141@student.rmit.edu.au>
2021-06-21 16:59:30 +1000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Bryan on 2021/6/21.
+//  Copyright © 2021 Bryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
